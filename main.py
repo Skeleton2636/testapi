eb186deffef815c13114c33b40e101782e4549be
Skeleton2636 <46952604+Skeleton2636@users.noreply.github.com>
2021-05-25 00:57:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
 
 app = FastAPI()
 
-fakedb = []
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
-class Course(BaseModel):
-    id: int
-    name: str
-    price: float
-    boolean: Optional[bool] = None
 
-@app.get('/')
-def read_root():
-    return {"greetings" : "Welcome"}
-
-@app.get('/courses')
-def get_courses():
-    return fakedb
-
-@app.get('/courses/{course_id}')
-def get_course_id(course_id: int):
-    course = course_id - 1
-    return fakedb[course]
-
-@app.post("/courses")
-def add_course(course: Course):
-    fakedb.append(course.dict())
-    return fakedb[-1]
-
-@app.delete("/courses/{course_id}")
-def delete_course(course_id: int):
-    fakedb.pop(course_id-1)
-    return {"Task": "Successful"}
+@app.get("/")
+def home():
+    return {"message":"Hello asdas}
