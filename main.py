3077375555836a517cea423ce30d00c472ddff01
Skeleton2636 <46952604+Skeleton2636@users.noreply.github.com>
2021-05-25 01:14:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,33 @@ app = FastAPI()
 #domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
+fakedb = []
+
+class Course(BaseModel):
+    id: int
+    name: str
+    price: float
+    boolean: bool
+
 @app.get("/")
 def home():
     return {"message":"Hello asdas}
+            
+@app.get('/courses')
+def get_courses():
+    return fakedb
+
+@app.get('/courses/{course_id}')
+def get_course_id(course_id: int):
+    course = course_id - 1
+    return fakedb[course]
+
+@app.post("/courses")
+def add_course(course: Course):
+    fakedb.append(course.dict())
+    return fakedb[-1]
+
+@app.delete("/courses/{course_id}")
+def delete_course(course_id: int):
+    fakedb.pop(course_id-1)
+    return {"Task": "Successful"}            
